5586e272af61de76c38a27308305e457b8125544
Antoine Blanchard <antoine@sand-lab-gpu.mit.edu>
2020-05-21 12:24:50 -0400

    update oscill

--- gpsearch/benchmarks/oscillator/run_benchmark.py
+++ gpsearch/benchmarks/oscillator/run_benchmark.py
@@ -1,8 +1,7 @@
 import numpy as np
-from gpsearch import (BlackBox, GaussianInputs, OptimalDesign,\
-                      Benchmarker, BenchmarkerLHS,\
-                      US, US_LW, IVR, IVR_LW, true_pdf, error_logpdf)
 from gpsearch.examples import Oscillator, Noise
+from gpsearch import (BlackBox, GaussianInputs, OptimalDesign,
+                      Benchmarker, BenchmarkerLHS, custom_KDE)
 
 
 def map_def(theta, oscil):
@@ -19,28 +18,42 @@ def main():
     u_init = [0, 0]
     noise = Noise([0, tf])
     oscil = Oscillator(noise, tf, nsteps, u_init)
-    myMap = BlackBox(map_def, args=(oscil,))
 
     mean, cov = np.zeros(ndim), np.ones(ndim)
     domain = [ [-6, 6] ] * ndim
     inputs = GaussianInputs(domain, mean, cov)
-    pt = true_pdf(inputs, "map_samples2D.txt")
 
     prefix = "oscill_" 
+    noise_var = 1e-3 
+    my_map = BlackBox(map_def, args=(oscil,), noise_var=noise_var)
 
-    n_init = 4
-    n_iter = 1#80
-    n_trials = 10#200
-    n_jobs = 10#20
+    n_init = 3
+    n_iter = 80
+    n_trials = 100
+    n_jobs = 30
 
-    metric = [ ("log_pdf", dict(pt=pt)) ]
-    acq_list = ["IVR", "US_LW", "IVR", "IVR_LW"] 
-    
+    # Compute true pdf
+    filename = "map_samples{:d}D.txt".format(ndim)
+    try:
+        smpl = np.genfromtxt(filename)
+        pts = smpl[:,0:-1]
+        yy = smpl[:,-1]
+    except:
+        pts = inputs.draw_samples(n_samples=100, sample_method="grd")
+        yy = my_map.evaluate(pts, parallel=True, include_noise=False)
+        np.savetxt(filename, np.column_stack((pts,yy)))
+    weights = inputs.pdf(pts)
+    pt = custom_KDE(yy, weights=weights)
+
+    metric = [ ("log_pdf", dict(pt=pt, pts=pts)) ]
+    acq_list = ["US", "US_LW", "IVR_IW", "IVR_LW"]
+
     for acq in acq_list:
-        b = Benchmarker(myMap, acq, n_init, n_iter, inputs, metric)
+        print("Benchmarking " + acq)
+        b = Benchmarker(my_map, acq, n_init, n_iter, inputs, metric)
         result = b.run_benchmark(n_trials, n_jobs, filename=prefix+acq)
 
-    b = BenchmarkerLHS(myMap, n_init, n_iter, inputs, metric)
+    b = BenchmarkerLHS(my_map, n_init, n_iter, inputs, metric)
     result = b.run_benchmark(n_trials, n_jobs, filename=prefix+"LHS")
 
 

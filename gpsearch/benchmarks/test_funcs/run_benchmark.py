771f5211cd6b174408313d1b78f7a94d687b1dd7
Antoine Blanchard <antoine@sand-lab-gpu.mit.edu>
2020-05-21 12:31:00 -0400

    update benchmark

--- gpsearch/benchmarks/test_funcs/run_benchmark.py
+++ gpsearch/benchmarks/test_funcs/run_benchmark.py
@@ -1,26 +1,28 @@
 import numpy as np
-from gpsearch import (OptimalDesign, Benchmarker)
-from gpsearch.examples import AckleyFunction
+from gpsearch import Benchmarker
+from gpsearch.examples import Ackley
 
 
 def main():
 
+    prefix = "ackley_"
     noise_var = 1e-3 
-
-    b = AckleyFunction(noise_var=noise_var, rescale_X=True)
+    b = Ackley(noise_var=noise_var, rescale_X=True)
     my_map, inputs, true_ymin, true_xmin = b.my_map, b.inputs, b.ymin, b.xmin
 
-    prefix = "ackley2d_noisevar1e-3_"
-
     n_init = 3
-    n_iter = 70
+    n_iter = 80
     n_trials = 100
     n_jobs = 40
 
-    metric = [ ("regret_tmap", dict(true_ymin=true_ymin,tmap=my_map)),
-               ("regret_obs", dict(true_ymin=true_ymin)) ]
+    metric = [ ("distmin_model", dict(true_xmin=true_xmin)) ,
+               ("distmin_obs"  , dict(true_xmin=true_xmin)) ,
+               ("regret_tmap"  , dict(true_ymin=true_ymin, tmap=my_map)) ,
+               ("regret_model" , dict(true_ymin=true_ymin)) ,
+               ("regret_obs"   , dict(true_ymin=true_ymin)) ]
 
-    acq_list = ["EI", "IVR_LWBO"]
+    acq_list = ["EI", "PI", "LCB", "LCB_LW",
+                "IVR", "IVR_BO", "IVR_LW", "IVR_LWBO"]
 
     for acq in acq_list:
         print("Benchmarking " + acq)

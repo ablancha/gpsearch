c252f4f843b59777fd64cd80dc8e575045b0a6bf
Antoine Blanchard <ablancha@mit.edu>
2020-10-18 08:05:41 -0400

    add pdf sampling

--- gpsearch/core/inputs.py
+++ gpsearch/core/inputs.py
@@ -2,7 +2,7 @@ import numpy as np
 import GPy
 from GPy.util.linalg import pdinv
 import pyDOE as doe
-from scipy.stats import uniform, lognorm
+from scipy.stats import uniform
 from .utils import process_parameters
 
 
@@ -36,11 +36,12 @@ class Inputs():
             Number of samples to generate. If sample_method="grd",
             number of grid points along each dimension. Otherwise, 
             total number of samples.
-        sample_method : {"lhs", "uni", "grd"}
+        sample_method : {"lhs", "uni", "grd", "pdf"}
             Method to generate the samples. 
                 - "lhs": Latin Hypercube Sampling
                 - "uni": Uniform Sampling
                 - "grd": Grid Sampling
+                - "pdf": Sample according to `self.pdf`
                 
         Returns
         -------
@@ -55,7 +56,7 @@ class Inputs():
             pts = self.rescale_samples(pts, self.domain)
 
         elif sample_method == "uni":
-            pts = uniform.rvs(size=(n_samples, self.input_dim))
+            pts = np.random.rand(n_samples, self.input_dim)
             pts = self.rescale_samples(pts, self.domain)
 
         elif sample_method == "grd":
@@ -63,6 +64,9 @@ class Inputs():
             grd = np.mgrid[ [slice(bd[0], bd[1], ngrid*1j) \
                              for bd in self.domain] ]
             pts = grd.T.reshape(-1, self.input_dim)
+
+        elif sample_method == "pdf":
+            pts = self.rvs(n_samples=n_samples)
 
         return pts
 
@@ -107,6 +111,22 @@ class Inputs():
         """
         raise NotImplementedError
 
+    def rvs(self, n_samples):
+        """Draw random samples according to `self.pdf`.
+
+        Parameters
+        ----------
+        n_samples : int
+            Number of samples.
+
+	Returns
+        -------
+        res : array
+            Random samples.
+
+        """
+        raise NotImplementedError
+
 
 class GaussianInputs(Inputs):
     """A class for Multivariate Gaussian inputs.
@@ -146,6 +166,10 @@ class GaussianInputs(Inputs):
         pdf = self.pdf(x)
         return - const * pdf[:,None]
 
+    def rvs(self, n_samples):
+        res = np.random.multivariate_normal(self.mu, self.cov, n_samples)
+        return res
+
 
 class UniformInputs(Inputs):
     """A class for Multivariate Uniform inputs.
@@ -170,6 +194,10 @@ class UniformInputs(Inputs):
 
     def pdf_jac(self, x):
         return np.zeros(x.shape)
+
+    def rvs(self, n_samples):
+        res = self.draw_samples(n_samples, sample_method="uni")
+        return res
 
 
 class LogNormalInputs(Inputs):
